333a43ca05d4ba029e11afdfb3bca9a9f57ba39b
Arto <Arto@DESKTOP-JQ3SDE4>
2025-07-18 08:07:02 +0300

    v 1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include "Geometry.hpp";
 
-int main() {
+int main(){
+    // Example usage of the Geometry library functions
+    
+    float hypot = Geometry::Math::FLO_Hypotenuse(3.0f, 4.0f);
+    float circle_area = Geometry::Geom::FLO_CircleArea(2.0f);
+    float octagon_perimeter = Geometry::Geom::FLO_OctagonPerimeter(1.0f);
+    std::cout << "Hypotenuse: " << hypot << std::endl;
+    std::cout << "Circle Area: " << circle_area << std::endl;
+    std::cout << "Octagon Perimeter: " << octagon_perimeter;
 
     return 0;
 }
